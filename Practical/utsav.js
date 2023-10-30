ce02427e363ed4d9b197ec4e047e91081755a2b0
UtsavRam <137594499+UtsavRam@users.noreply.github.com>
2023-10-30 11:54:29 +0530

    .

---
+++ Practical/utsav.js
@@ -0,0 +1,11 @@
+ 
+ document.write("<u> heloo heloo </u>");
+
+ document.getElementById("para").innerText = "hello para"
+
+
+
+//   window.alert("i'm alert");
+//   window.prompt(" youre name");
+//   window.confirm(" are you sure");
+//   window.print();
